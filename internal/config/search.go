05407ae5d2173e501108166d1e51b9fe5054f46e
gtalarico <gtalarico.dev@gmail.com>
2019-01-27 17:09:14 -0800

    Added function comments

--- internal/config/search.go
+++ internal/config/search.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 )
 
+// Searches projects in config, matches by strings.Contains, returns list of all matching project
 func SearchProjects(query string, config Config) []*Project {
 	var projects []*Project
 	for _, project := range config.Projects {
@@ -16,6 +17,7 @@ func SearchProjects(query string, config Config) []*Project {
 	return projects
 }
 
+// Searches projects in config, returns first exact match or nil
 func MatchProjects(query string, config Config) (project *Project) {
 	for _, p := range config.Projects {
 		if p.Name == query {
@@ -25,6 +27,7 @@ func MatchProjects(query string, config Config) (project *Project) {
 	return
 }
 
+// Gets on exact project using SearchProjects, if none or multiples error is raised
 func GetOneProject(query string, config Config) (project *Project, err error) {
 	projects := SearchProjects(query, config)
 	numProjects := len(projects)
